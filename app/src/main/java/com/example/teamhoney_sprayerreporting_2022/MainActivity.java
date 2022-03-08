58219dce891e5003af587477c8e247a2ded8763a
Jacob Dixon <jake.b.dixon@gmail.com>
2022-03-08 13:34:24 -0600

    Added username box, password box, Sign in button, logo, title

--- app/src/main/java/com/example/teamhoney_sprayerreporting_2022/MainActivity.java
+++ app/src/main/java/com/example/teamhoney_sprayerreporting_2022/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.teamhoney_sprayerreporting_2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private EditText usernameView;
+    private EditText passwordView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameView = findViewById(R.id.usernameBox);
+        passwordView = findViewById(R.id.passwordBox);
+    }
+
+    public void close(View view){
+        closeKeyboard();
+    }
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
