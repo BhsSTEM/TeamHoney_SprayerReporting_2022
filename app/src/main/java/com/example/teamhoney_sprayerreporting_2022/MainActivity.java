351124964bd69f6843cdad81f4c60ea912e5f1c4
Jacob Dixon <jake.b.dixon@gmail.com>
2022-03-10 22:32:58 -0500

    Added code so username and password input work

--- app/src/main/java/com/example/teamhoney_sprayerreporting_2022/MainActivity.java
+++ app/src/main/java/com/example/teamhoney_sprayerreporting_2022/MainActivity.java
@@ -7,16 +7,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity{
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity{
     private TextInputLayout passwordView;
     private String username = "";
     private String password = "";
+    private ArrayList<String> usernames = new ArrayList<>();
+    private ArrayList<String> passwords = new ArrayList<>();
 
 
     @Override
@@ -32,6 +37,8 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         usernameView = findViewById(R.id.usernameBox);
         passwordView = findViewById(R.id.passwordBox);
+        usernames.add("admin");
+        passwords.add("password");
     }
 
     public void close(View view){
@@ -52,15 +59,26 @@ public class MainActivity extends AppCompatActivity{
         password = passwordView.getEditText().getText().toString();
         Log.d("test", username);
         Log.d("test", password);
-        //Code to check username and password in .csv
-        Scanner sc = null;
-        try {
-            sc = new Scanner(new File("test"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        //Code to check username and password
+        if(TextUtils.isEmpty(usernameView.getEditText().getText().toString()) || TextUtils.isEmpty(passwordView.getEditText().getText().toString())){
+            Toast.makeText(MainActivity.this, "Please enter username and password", Toast.LENGTH_LONG).show();
         }
-        if (sc != null) {
-            sc.close();
+        else{
+            for(int i = 0;i<usernames.size();i++){
+                if(usernameView.getEditText().getText().toString().equals(usernames.get(i))){
+                    if(passwordView.getEditText().getText().toString().equals(passwords.get(i))){
+                        Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Invalid Password", Toast.LENGTH_LONG).show();
+                    }
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Username not found", Toast.LENGTH_LONG).show();
+                }
+            }
         }
+
+
     }
 }
