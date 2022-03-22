868d0a079a3f3f04cbe63e7ab0b87e7a8385b541
TarunVedula <98503523+TarunVedula@users.noreply.github.com>
2022-03-22 13:30:35 -0500

    Admin permissions

---
+++ app/src/main/java/com/example/teamhoney_sprayerreporting_2022/DatabaseHelper.java
@@ -0,0 +1,23 @@
+//package com.example.teamhoney_sprayerreporting_2022;
+//
+//import android.database.sqlite.SQLiteDatabase;
+//import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class DatabaseHelper extends SQLiteOpenHelper {
+//
+//    private static final String DB_NAME = "Users.db";
+//    private static final String DB_TABLE = "Users_Table";
+//
+//    private static final String ID = "ID";
+//    private static final String NAME = "NAME";
+//
+//    private static final String CREATE_TABLE = "CREATE TABLE "+ DB_TABLE +" ("+ID + " INTEGER PRIMARY KEY AUTOINCREMENT "+NAME+ " TEXT "+ ")";
+//
+//
+//
+//
+////    public DatabaseHelper(Context context){
+////        super(context);
+////    }
+//
+//}
